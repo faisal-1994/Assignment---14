12d7ce58488305dead20ccad832b6a8cb688c05c
faisal-1994 <125819206+faisal-1994@users.noreply.github.com>
2023-07-15 20:48:43 +0600

    Update Content.jsx

--- Content.jsx
+++ Content.jsx
@@ -1,33 +1,23 @@
 
 import { useRef } from 'react';
 
-const Content = ()=>{
-
-    
+const Content = ()=>{  
     let textarea = useRef();
     let tagP = useRef();
     
-    const demo = ()=>{
-        
+    const demo = ()=>{    
         const  valueOfTextarea = textarea.current.value;
-        tagP.current.innerHTML= valueOfTextarea;
-        
-    }
- 
-
+        tagP.current.innerHTML= valueOfTextarea;       
+    } 
+    
 return(
         <div>
             
                 <textarea name="" id="" cols="30" rows="5"  ref={textarea}></textarea> <br />
             
-                <button onClick={demo} style={{backgroundColor:"white", color:"black", marginTop:"30px"}}>Submit
-                </button> 
+                <button onClick={demo} style={{backgroundColor:"white", color:"black", marginTop:"30px"}}>Submit </button> 
                   
-        
-            <p ref={tagP}>
-            </p>
-
-
+                <p ref={tagP}> </p>
            
         </div>
     )
